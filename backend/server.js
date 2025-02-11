dc4a26263bfa4cdc8441575a8b32b8bcbea6d5df
edisonCurrentUser <edison.tipan9202@utc.edu.ec>
2025-02-11 08:23:09 -0500

    Prueba12

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,6 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => {
   res.send('¡Bienvenido al backend!');
 });
-  
 
 // Rutas
 app.use('/api/certificados', certificadosRoutes);
